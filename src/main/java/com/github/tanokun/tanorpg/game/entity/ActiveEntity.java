53df97d0acfbc86fca203bfb917a82e6b3013ce6
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-06 18:46:12 +0900

    [fix] 体力の追加

--- src/main/java/com/github/tanokun/tanorpg/game/entity/ActiveEntity.java
+++ src/main/java/com/github/tanokun/tanorpg/game/entity/ActiveEntity.java
@@ -1,6 +1,7 @@
 package com.github.tanokun.tanorpg.game.entity;
 
 import com.github.tanokun.tanorpg.game.entity.base.ObjectEntity;
+import com.github.tanokun.tanorpg.player.status.StatusType;
 import org.bukkit.entity.Entity;
 
 public class ActiveEntity {
@@ -8,12 +9,24 @@ public class ActiveEntity {
 
     private final Entity entity;
 
+    private int hasHP;
+
     public ActiveEntity(ObjectEntity objectEntity, Entity entity) {
         this.objectEntity = objectEntity;
         this.entity = entity;
+        this.hasHP = objectEntity.getStatusMap().getStatus(StatusType.HP);
     }
 
     public ObjectEntity getObjectEntity() {
         return objectEntity;
+    }
+
+    public int getHasHP() {
+        return hasHP;
+    }
+
+    public int setHasHP(int hasHP) {
+        this.hasHP = hasHP;
+        return this.hasHP;
     }
 }
