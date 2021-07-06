d345e10148013b70b22601209c9fd7c587c885f8
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-06 18:46:56 +0900

    [fix] 終了処理の追加

--- src/main/java/com/github/tanokun/tanorpg/player/quest/listener/ActionQuestListener.java
+++ src/main/java/com/github/tanokun/tanorpg/player/quest/listener/ActionQuestListener.java
@@ -2,11 +2,19 @@ package com.github.tanokun.tanorpg.player.quest.listener;
 
 import com.github.tanokun.tanorpg.TanoRPG;
 import com.github.tanokun.tanorpg.player.Member;
+import com.github.tanokun.tanorpg.player.quest.Quest;
 import com.github.tanokun.tanorpg.player.quest.inv.QuestListMenu;
+import com.github.tanokun.tanorpg.util.ItemUtils;
+import com.github.tanokun.tanorpg.util.io.MapNode;
+import com.github.tanokun.tanorpg.util.smart_inv.inv.ClickableItem;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
 
 public class ActionQuestListener implements Listener {
 
@@ -16,7 +24,26 @@ public class ActionQuestListener implements Listener {
         if (m == null) return;
 
         if (m.getQuestMap().isAction()) return;
+
         if (TanoRPG.getPlugin().getQuestManager().getQuests(e.getNPC().getId()) != null){
+            if (m.getQuestMap().getActiveQuest() != null && m.getQuestMap().getActiveQuest().getNpcId() == e.getNPC().getId()) {
+                if (!m.getQuestMap().getActiveQuest().isClear()) return;
+                Quest quest = m.getQuestMap().getActiveQuest().getQuest();
+                m.getQuestMap().setAction(true);
+                Bukkit.getScheduler().runTaskAsynchronously(TanoRPG.getPlugin(), () -> {
+                    quest.getShowQuestActions()
+                            .forEach(a -> a.execute(m));
+                    Bukkit.getScheduler().runTask(TanoRPG.getPlugin(), () -> {
+                        m.getQuestMap().setAction(false);
+                        m.getQuestMap().addClearQuest(m.getQuestMap().getActiveQuest());
+                        m.getQuestMap().setActiveQuest(new MapNode<>(null, 0));
+                        m.getQuestMap().removeQuest(quest.getName());
+                        e.getClicker().sendMessage(TanoRPG.PX + "クエスト「" + quest.getName() + "」をクリアしました！");
+                        e.getClicker().setCompassTarget(TanoRPG.getPlugin().getDataManager().getStartLoc());
+                    });
+                });
+                return;
+            }
             QuestListMenu.getInv(e.getNPC(), e.getClicker()).open(e.getClicker());
             TanoRPG.playSound(e.getClicker(), Sound.ENTITY_SHULKER_OPEN, 3, 1);
         }
