4f67ffd58f012e3028358cc0efa370d827dc517e
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-07-11 19:11:21 +0900

    [add] ルーン設定(本画面)の追加

---
+++ src/main/java/com/github/tanokun/tanorpg/game/item/inv/RuneInventoryMenu.java
@@ -0,0 +1,35 @@
+package com.github.tanokun.tanorpg.game.item.inv;
+
+import com.github.tanokun.tanorpg.game.item.type.base.ItemData;
+import com.github.tanokun.tanorpg.util.ItemUtils;
+import com.github.tanokun.tanorpg.util.smart_inv.inv.SmartInventory;
+import com.github.tanokun.tanorpg.util.smart_inv.inv.contents.InventoryContents;
+import com.github.tanokun.tanorpg.util.smart_inv.inv.contents.InventoryProvider;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class RuneInventoryMenu implements InventoryProvider {
+    private ItemStack item;
+    private ItemData itemData;
+
+    public SmartInventory getInv(){
+        return SmartInventory.builder()
+                .id("RuneInventory")
+                .title("§b§lアイテム選択")
+                .update(false)
+                .provider(this)
+                .size(3, 9)
+                .build();
+    }
+
+    public RuneInventoryMenu(ItemStack itemStack) {
+        item = itemStack;
+        itemData = ItemUtils.getItemData(itemStack);
+    }
+
+
+    @Override
+    public void init(Player player, InventoryContents contents) {
+
+    }
+}
